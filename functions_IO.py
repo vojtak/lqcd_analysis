62d5dfa9c8842b5fcdecf592ea70896ba5cdf4e2
Vojtech Krejcirik <vojtakk@gmail.com>
2015-02-12 15:41:11 +0900

    loading propagators, plotting

---
+++ functions_IO.py
@@ -0,0 +1,84 @@
+
+# import modules
+
+from subprocess import check_output  # calling bash
+
+
+# -----------------------------------------------------
+# extracting propagators
+
+def extract_propagators(path_results, measurement, particles, single_conf=0, single_t_slice=0):
+    """
+    extract propagators for a given measurement, and particles
+    arguments : path_results - path to result folder
+                measurements - type of measurement
+                particles    - list of particles (always as a list)
+                single_conf (optional)    - if = 1, only one configuration
+                single_t_slice (optional) - if = 1, only one time slice
+    
+    output is a tuple of a format    (
+                                     [particle]
+                                     [configuration]
+                                     [measurements within one configuration (various source positions,
+                                                                             for example)]
+                                     [propagator itself as a complex number]
+                                     ,
+                                     list of particles, list of configurations
+                                     )
+    """
+    particles_in=[par for par in particles]
+    
+    configurations = [name for name in check_output(["ls",path_results]).split("\n")[:-1]]
+    if single_conf:
+      configurations=[configurations[0]]
+
+    folder_paths=[path_results+conf+"/"+measurement+"/" for conf in  configurations]
+
+    for particle in particles:
+      for con,folder_con in enumerate(folder_paths):
+        if particle not in check_output(["ls",folder_con]):
+          print "ERROR, particle", particle, "not in", configurations[con]
+          print "       continue without", particle
+          particles_in.remove(particle)
+          break
+          if len(particles_in)==0:
+            print "ERROR, not enough particles to work with"
+            return -1
+
+          
+    particle_paths=[
+                    [
+                     [folder_con+name for name in check_output(["ls",folder_con]).split("\n")[:-1]
+                       if particle in name]
+                     for folder_con in folder_paths] 
+                    for particle in particles_in]
+    if single_t_slice:
+      particle_paths=[
+                       [[slices[0]] for slices in confs]
+                     for confs in particle_paths] 
+                    
+    
+    propagators_all=[
+                     [
+                      [
+                       read_propagator(particle_paths[particle][conf][prop])  
+                         for prop in range(len(particle_paths[particle][conf]))]
+                     for conf in range(len(particle_paths[particle]))]
+                    for particle in range(len(particle_paths))]
+                   
+    return (propagators_all, particles_in, configurations)
+    
+
+
+# -----------------------------------------------------
+# reading from file
+
+def read_propagator(file_path):
+    """
+    read propagator from a file at file_path
+    """
+    return [complex(float(line.split('\t')[1].split(' ')[0]),float(line.split('\t')[1].split(' ')[1]))
+            for line in open(file_path,'rb').readlines()
+           ]
+             
+
---
+++ functions_Physics.py
@@ -0,0 +1,26 @@
+
+# import modules
+
+import numpy
+from cmath import *
+
+
+
+
+
+
+# -----------------------------------------------------
+# effective mass
+
+def effective_mass(propagator):
+    """
+    calculate efective mass from the propagator
+    """
+    len_p=len(propagator)
+    output=len_p*[None]
+    for i in range(len_p/2-1):
+        output[i]=(log(propagator[i]/propagator[i+1])).real
+    for i in range(-len_p/2+1,0):
+        output[i]=(log(propagator[i+1]/propagator[i])).real
+    
+    return output
